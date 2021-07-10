60a0fc06a21644478084e62f57a97cb6f0b88b2b
shahriar201 <shahriarbrine201@gmail.com>
2021-07-10 11:05:49 +0600

    Employee attendance and fee update

--- app/User.php
+++ app/User.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use\App\Model\Designation;
+use App\Model\Designation;
 
 class User extends Authenticatable
 {
